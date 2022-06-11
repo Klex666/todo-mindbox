4e0c4cbc466df92b614c893e69741dbe65350692
Klex666 <57945831+Klex666@users.noreply.github.com>
2022-06-11 21:41:44 +0400

    add: active button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,28 @@
 import { useActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 import './Header.scss';
 
 const Header = () => {
+  const { sort } = useTypedSelector((state) => state.headerReducer);
   const { setSort } = useActions();
 
   return (
     <div className="header">
       <div className="buttons-list">
-        <button className="button" onClick={() => setSort('all')}>
+        <button
+          className={sort === 'all' ? 'button active' : 'button'}
+          onClick={() => setSort('all')}>
           Все
         </button>
-        <button className="button" onClick={() => setSort('active')}>
+        <button
+          className={sort === 'active' ? 'button active' : 'button'}
+          onClick={() => setSort('active')}>
           Активные
         </button>
-        <button className="button" onClick={() => setSort('completed')}>
+        <button
+          className={sort === 'completed' ? 'button active' : 'button'}
+          onClick={() => setSort('completed')}>
           Выполненые
         </button>
       </div>
